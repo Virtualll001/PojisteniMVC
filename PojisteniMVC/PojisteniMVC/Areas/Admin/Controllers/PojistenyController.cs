7ec6d605691b088073950b3119c7091d0b82d26a
Kateřina Mlázovská <60098902+Virtualll001@users.noreply.github.com>
2021-10-16 19:43:21 +0200

    4 - začátek Controllers/Views
    dokončena lekce 80

---
+++ PojisteniMVC/PojisteniMVC/Areas/Admin/Controllers/PojistenyController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PojisteniMVC.DataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PojisteniMVC.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class PojistenyController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public PojistenyController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll() //vrátí všechny pojištěné v JSON formátu
+        {
+            var allObj = _unitOfWork.Pojisteny.GetAll();
+            return Json(new { data = allObj });
+        }
+        #endregion
+    }
+}
