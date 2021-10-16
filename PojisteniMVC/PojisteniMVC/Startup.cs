5e09479c4a45dede51c30937dda0a14e44b039b1
Kateřina Mlázovská <60098902+Virtualll001@users.noreply.github.com>
2021-10-16 08:14:50 +0200

    2 - Úprava Areas
    dokončení lekce 58

--- PojisteniMVC/PojisteniMVC/Startup.cs
+++ PojisteniMVC/PojisteniMVC/Startup.cs
@@ -57,7 +57,7 @@ namespace PojisteniMVC
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
